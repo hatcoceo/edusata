4e1a021387fc7cf66c5d21ab755f56edfe764f7b
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2018-11-10 09:28:07 +0700

    Add files via upload

---
+++ formedu.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <title>form edusata</title>
+    </head> 
+    <body> 
+ 
+ 	    <form action="xulyformedu.php" method="post">
+            <table width="1200" border="2"  align="center">  
+                <tr> 
+                  <td>đối lập</td>
+                  <td>không cần</td>
+                  <td>tăng giảm</td>
+                  <td>thay đổi</td>
+                  <td>phân loại</td>
+                  <td>kết hợp</td>
+                  <td>liên tưởng</td>
+                  <td>tpct</td>
+                  <td>hiện tại</td>
+                  <td>nhận định</td>
+                  <td>khác</td>
+                </tr>
+                <tr>
+	                <td>
+		              <input type="text" name="doi_lap" size="5">
+	                </td>
+	                <td>
+		              <input type="text" name="khong_can" size="5">
+	                </td>
+	                <td>
+		             <input type="text" name="tang_giam" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="thay_doi" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="phan_loai" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="ket_hop" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="lien_tuong" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="tpct" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="hien_tai" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="nhan_dinh" size="5">
+	                </td>
+	                <td>
+		               <input type="text" name="khac" size="5">
+	                </td>
+	            </tr>
+                <tr>
+	                <td colspan="11" align="center">
+	                 <input type="reset" name="reset" value="đặt lại">&nbsp &nbsp 
+	                 <input type="submit" name="submit" value="gửi đi">
+	                </td>
+                </tr>
+          </table>
+        </form>
+    </body>
+</html>
